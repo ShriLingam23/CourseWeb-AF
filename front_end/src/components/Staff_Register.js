102b4dc8f55420d2883fbfef96b38c593b3afa9e
ShriLingam23 <trnshri23@gmail.com>
2019-05-08 00:29:39 +0530

    Instaructor Insertion and Deletion operation

--- front_end/src/components/Staff_Register.js
+++ front_end/src/components/Staff_Register.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 import axios from 'axios';
 import {Link} from 'react-router-dom'
-import { Collapse, Button } from 'reactstrap';
+import { Alert } from 'reactstrap';
 
 import logo from '../components/logo.svg'
 import { MdEmail } from "react-icons/md";
@@ -18,7 +18,7 @@ class Staff_Register extends Component{
         super(props);
         // this.toggle = this.toggle.bind(this);
         this.state = { 
-            // collapse: false,
+            visible: false,
             fullName:'',
             email:'',
             password:'',
@@ -31,11 +31,26 @@ class Staff_Register extends Component{
 
         this.onFormSubmit= this.onFormSubmit.bind(this);
         this.onValueChange = this.onValueChange.bind(this);
+
+        this.onDismiss = this.onDismiss.bind(this);
+        this.newlyAdded = this.newlyAdded.bind(this);
     }
 
-    // toggle() {
-    //     this.setState(state => ({ collapse: !state.collapse }));
-    // }
+    onDismiss() {
+        this.setState({ visible: false });
+    }
+
+    newlyAdded(){
+        if(this.state.visible){
+            return (
+                <div>
+                    <Alert color="primary" isOpen={this.state.visible} toggle={this.onDismiss} fade={false}>
+                        I am a primary alert and I can be dismissed without animating!
+                    </Alert>
+                </div>
+            );
+        }
+    }
 
     onValueChange(e){
         this.setState({
@@ -67,48 +82,31 @@ class Staff_Register extends Component{
 
         axios.post('http://localhost:4000/staff/add',staff)
             .then(
-                res=>console.log(res.data),
+                res=>{
+                    console.log(res.data)
+                    // document.getElementById('staffForm').reset()
+                    this.setState({
+                        visible:true,
+                        fullName:'',
+                        email:'',
+                        password:'',
+                        profession:'',
+                        contactNum:'',
+                        location:'',
+                        response:''});
+
+                },
                 err=>console.log(err)
             )
 
-        document.getElementById('staffForm').reset()
+        
     }
 
     render(){
         return(
             <div className="container" style={{paddingTop:'20px'}}>
-                {/* <header>
-                    
-                    <div className="navbar navbar-dark bg-dark shadow-sm">
-                        <div className="container d-flex justify-content-between">
-                            <Link className="navbar-brand" to='/'>
-                                <img src={logo} width="30" height="30" className="d-inline-block align-top" alt=""/>
-                                Loops' Student Portal
-                            </Link>
-
-                            <Button className="navbar-toggler-icon" color="primary" onClick={this.toggle} ></Button>
-                            <Collapse isOpen={this.state.collapse}>
-                                <div className="container">
-                                    <div className="row">
-                                        <div className="col-sm-8 col-md-7 py-4">
-                                            <h4 className="text-white">About</h4>
-                                            <p className="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
-                                        </div>
-                                        <div className="col-sm-4 offset-md-1 py-4">
-                                            <h4 className="text-white">Contact</h4>
-                                            <ul className="list-unstyled">
-                                                <li><a href="#" className="text-white">Follow on Twitter</a></li>
-                                                <li><a href="#" className="text-white">Like on Facebook</a></li>
-                                                <li><a href="#" className="text-white">Email me</a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                            </Collapse>
-                        </div>
-                    </div>
-                </header> */}
-
+                
+                {this.newlyAdded()}
                 {/* <!--Body--> */}
 
                 <main role="main" style={{marginTop:'10px'}}>
--- front_end/src/components/Staff_View.js
+++ front_end/src/components/Staff_View.js
@@ -1,45 +1,100 @@
 import React,{Component} from 'react';
+import axios from 'axios';
+import { Alert,UncontrolledAlert,Button } from 'reactstrap';
+import {Link} from 'react-router-dom'
+
+import StaffTable from './Staff_Table';
 
 class Staff_View extends Component{
 
-    render(){
-        return(
-            <div className='container' style={{marginTop:'20px'}}>
-            
-                <div className='card'>
-                <table class="table table-hover table-responsive-md table-striped">
-                <thead style={{backgroundColor:'#bdbdbd'}}>
-                    <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    </tr>
-                    <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    </tr>
-                    <tr>
-                    <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
-                    <td>@twitter</td>
-                    </tr>
-                </tbody>
-                </table>
-                </div>
+    constructor(props){
+        super(props);
 
-            </div>
-        )
+        this.state={
+            staffs:[]
+        }
+
+        this.fillTable=this.fillTable.bind(this);
+        this.checkData= this.checkData.bind(this);
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:4000/staff/')
+            .then(
+                staffs=>this.setState({staffs:staffs.data})
+            )
+
+            //console.log(this.state.staffs.length)
+    }
+
+    componentWillUpdate(){
+        axios.get('http://localhost:4000/staff/')
+            .then(
+                staffs=>this.setState({staffs:staffs.data})
+            )
+    }
+
+    fillTable(){
+
+        return this.state.staffs.map(staff=>{
+            return <StaffTable key={staff._id} staff={staff}/>
+        })
+    }
+
+    checkData(){
+        if(this.state.staffs.length>0){
+
+            return(
+                <div className='container' style={{marginTop:'20px'}}>
+                
+                    <div className='card'>
+                    <table className="table table-hover table-responsive-md table-striped" style={{marginTop:'5px',marginBottom:'5px'}}>
+                        <thead style={{backgroundColor:'#bdbdbd'}}>
+                            <tr>
+                                <th scope="col">Staff Name</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Profession</th>
+                                <th scope="col">Contact Number</th>
+                                <th scope="col">Location</th>
+                                <th scope="col" colSpan='2'></th>
+                            </tr>
+                        </thead>
+                    <tbody>
+                        {this.fillTable()}
+                    </tbody>
+                    </table>
+                    </div>
+    
+                </div>
+            )
+
+        }
+        else{
+            return(
+                <div className='container' style={{marginTop:'150px'}}>
+                    <UncontrolledAlert color="warning">
+                        <h4 className="alert-heading">No Data Available</h4>
+                        <p>
+                        Aww yeah, you successfully read this important alert message. This example text is going
+                        to run a bit longer so that you can see how spacing within an alert works with this kind
+                        of content.
+                        </p>
+                        <hr />
+                        <p className="mb-0">
+                        Whenever you need to, be sure to use margin utilities to keep things nice and tidy.
+                        </p>
+                    </UncontrolledAlert>
+                    <br></br>
+                    <div style={{display:'flex',justifyContent:'center'}}>
+                        <Link to='/staff/add'><Button color="info">Add a New Staff</Button></Link>
+                    </div>
+                </div>
+            )
+        }
+    }
+
+    render(){
+        return this.checkData()
     }
 
 
