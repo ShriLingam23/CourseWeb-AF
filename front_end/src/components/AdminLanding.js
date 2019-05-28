0d0945ae026c053592cc3fb407133ff657e2ccaa
ShriLingam23 <trnshri23@gmail.com>
2019-05-28 21:06:34 +0530

    improve code quality

--- front_end/src/components/AdminLanding.js
+++ front_end/src/components/AdminLanding.js
@@ -10,53 +10,11 @@ import '../assets/css/Landing.css'
 import Slide from './Slides'
 
 class AdminLanding extends Component{
-
-    // constructor(props) {
-    //     super(props);
-    //     this.toggle = this.toggle.bind(this);
-    //     this.state = { collapse: false };
-    // }
-
-    // toggle() {
-    //     this.setState(state => ({ collapse: !state.collapse }));
-    // }
     
 
     render(){
         return (
             <div className="container" style={{paddingTop:'20px'}}>
-                {/* <header>
-                    
-                    <div className="navbar navbar-dark bg-dark shadow-sm">
-                        <div className="container d-flex justify-content-between">
-                            <a className="navbar-brand" href="#">
-                                <img src={logo} width="30" height="30" className="d-inline-block align-top" alt=""/>
-                                Loops' Student Portal
-                            </a>
-
-                            <Button className="navbar-toggler-icon" color="primary" onClick={this.toggle} ></Button>
-                            <Collapse isOpen={this.state.collapse}>
-                                <div className="container">
-                                    <div className="row">
-                                        <div className="col-sm-8 col-md-7 py-4">
-                                            <h4 className="text-white">About</h4>
-                                            <p className="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
-                                        </div>
-                                        <div className="col-sm-4 offset-md-1 py-4">
-                                            <h4 className="text-white">Contact</h4>
-                                            <ul className="list-unstyled">
-                                                <li><a href="#" className="text-white">Follow on Twitter</a></li>
-                                                <li><a href="#" className="text-white">Like on Facebook</a></li>
-                                                <li><a href="#" className="text-white">Email me</a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                            </Collapse>
-                        </div>
-                    </div>
-                </header> */}
-
 
                  {/* <!--Body--> */}
 
