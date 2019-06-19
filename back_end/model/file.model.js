8aa4929ac0bea1a5246ae963cbe0e87cf73e9067
ShriLingam23 <trnshri23@gmail.com>
2019-06-19 16:11:25 +0530

    File db model

---
+++ back_end/model/file.model.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var FileSchema = new Schema({
+    file: { data: Buffer, contentType: String}
+}, {
+    timestamps: true
+});
+
+//So the collection name will be files, since we have used "File" as model name
+module.exports = mongoose.model('File', FileSchema);
